01d5124c719960db9e1ed6f0a20cb4c243593d4e
Leonel Gauna <leonel.mariano.g@gmail.com>
2020-04-15 17:13:23 -0300

    Removed unnecessary export
    I don't need to export the function "removeAllActive" since it's not being used in the main script.

--- js/ui.js
+++ js/ui.js
@@ -1,11 +1,11 @@
-export function removeAllActive() {
+function removeAllActive() {
   document.querySelectorAll('.btn').forEach((btn) => {
     btn.classList.remove('active-bg');
     btn.classList.remove('active-no-bg');
   });
 }
 
-export function addActiveClass(e) {
+export default function addActiveClass(e) {
   removeAllActive();
   if (e.currentTarget.parentNode.parentNode.id === 'menu-1' || e.currentTarget.parentNode.id === 'instalar-app') {
     e.currentTarget.classList.add('active-bg');
